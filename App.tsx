c250aa3c1e6318e1a0afbb48b5b8fc7240e1ee31
Magin Suarez <33673860+albertomaginsuarez@users.noreply.github.com>
2020-09-21 12:09:49 -0300

    first

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
+
+import CameraDetection from './CameraDetection';
+
+export default function App() {
+
+  
+    
+    return (
+      <CameraDetection/>
+    );
+  
+}
